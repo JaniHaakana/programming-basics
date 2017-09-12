616100854e0c2cff46e698cc96bba926fc91f5f2
JaniHaakana <jani.haakana@student.saimia.fi>
2017-09-12 13:12:35 +0300

    Finished Task1

---
+++ conditional-statements/Task1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Prompt user
+            Console.Write("Please enter a number:");
+
+            // Read user input
+            String userInput;
+            userInput = Console.ReadLine();
+
+            //Evaluate user input
+            int evaluatedNumber;
+            int.TryParse(userInput, out evaluatedNumber);
+
+            if(evaluatedNumber > 0)
+            {
+                Console.WriteLine("Number {0} is greater than 0", evaluatedNumber);
+            }
+            else if(evaluatedNumber < 0)
+            {
+                Console.WriteLine("Number {0} is less than 0", evaluatedNumber);
+            }
+            else
+            {
+                Console.WriteLine("Number {0} equals 0", evaluatedNumber);
+            }
+
+            // Wait for user input
+            Console.ReadKey(); 
+        }
+    }
+}
