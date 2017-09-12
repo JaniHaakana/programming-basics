02a9e8818eb8573c950bbd7d9c5e362c6ba666f6
JaniHaakana <jani.haakana@student.saimia.fi>
2017-09-12 14:49:55 +0300

    Started Task4

---
+++ conditional-statements/Task4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Prompt user
+            Console.Write("Please enter a number:");
+
+            // Read user input
+            String userInput;
+            userInput = Console.ReadLine();
+
+            //Evaluate user input
+            int evaluatedNumber;
+            int.TryParse(userInput, out evaluatedNumber);
+
+            
+
+            // Wait for user input
+            Console.ReadKey();
+        }
+    }
+}
