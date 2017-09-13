b019478ee788b3a1118c661f271edd7d5c4402aa
JaniHaakana <jani.haakana@student.saimia.fi>
2017-09-13 10:35:40 +0300

    Finished Task4

--- conditional-statements/Task4/Program.cs
+++ conditional-statements/Task4/Program.cs
@@ -7,17 +7,91 @@ namespace Task4
         static void Main(string[] args)
         {
             // Prompt user
-            Console.Write("Please enter a number:");
+            Console.Write("Please enter a number 1:");
 
             // Read user input
             String userInput;
             userInput = Console.ReadLine();
 
             //Evaluate user input
-            int evaluatedNumber;
-            int.TryParse(userInput, out evaluatedNumber);
+            int evaluatedNumber1;
+            int.TryParse(userInput, out evaluatedNumber1);
 
-            
+            // Prompt user
+            Console.Write("Please enter a number 2:");
+
+            userInput = Console.ReadLine();
+
+            //Evaluate user input
+            int evaluatedNumber2;
+            int.TryParse(userInput, out evaluatedNumber2);
+
+            // Prompt user
+            Console.Write("Please enter a number 3:");
+
+            userInput = Console.ReadLine();
+
+            //Evaluate user input
+            int evaluatedNumber3;
+            int.TryParse(userInput, out evaluatedNumber3);
+
+
+            if (evaluatedNumber1 > evaluatedNumber2)
+            {
+                if (evaluatedNumber1 > evaluatedNumber3)
+                {
+                    if (evaluatedNumber2 > evaluatedNumber3)
+                    {
+                        Console.WriteLine("1. Number {0} \n2. Number {1} \n3. Number {2}", evaluatedNumber3, evaluatedNumber2, evaluatedNumber1);
+                    }
+                    else
+                    {
+                        Console.WriteLine("1. Number {0} \n2. Number {1} \n3. Number {2}", evaluatedNumber2, evaluatedNumber3, evaluatedNumber1);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("1. Number {0} \n2. Number {1} \n3. Number {2}", evaluatedNumber2, evaluatedNumber1, evaluatedNumber3);
+                }
+            }
+
+            else if (evaluatedNumber2 > evaluatedNumber1)
+            {
+                if (evaluatedNumber2 > evaluatedNumber3)
+                {
+                    if (evaluatedNumber3 > evaluatedNumber1)
+                    {
+                        Console.WriteLine("1. Number {0} \n2. Number {1} \n3. Number {2}", evaluatedNumber1, evaluatedNumber3, evaluatedNumber2);
+                    }
+                    else
+                    {
+                        Console.WriteLine("1. Number {0} \n2. Number {1} \n3. Number {2}", evaluatedNumber3, evaluatedNumber1, evaluatedNumber2);
+                    }  
+                }
+                else
+                {
+                    Console.WriteLine("1. Number {0} \n2. Number {1} \n3. Number {2}", evaluatedNumber1, evaluatedNumber2, evaluatedNumber3);
+                }
+            }
+
+            else if (evaluatedNumber3 > evaluatedNumber1)
+            {
+                if (evaluatedNumber3 > evaluatedNumber2)
+                {
+                    if (evaluatedNumber1 > evaluatedNumber2)
+                    {
+                        Console.WriteLine("1. Number {0} \n2. Number {1} \n3. Number {2}", evaluatedNumber2, evaluatedNumber1, evaluatedNumber3);
+                    }
+                    else
+                    {
+                        Console.WriteLine("1. Number {0} \n2. Number {1} \n3. Number {2}", evaluatedNumber1, evaluatedNumber2, evaluatedNumber3);
+                    }                   
+                }
+                else
+                {
+                    Console.WriteLine("1. Number {0} \n2. Number {1} \n3. Number {2}", evaluatedNumber1, evaluatedNumber3, evaluatedNumber2);
+                }
+            }
 
             // Wait for user input
             Console.ReadKey();
